dfa443b73b7a3858db14467d482080c2daba7919
elamparethi <elamparethi02a2gmail.com>
2023-02-15 22:09:58 +0530

    Added: CompanyEmployeeWage java program

---
+++ CompanyEmployeeWage.java
@@ -0,0 +1,13 @@
+package com.employee;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class CompanyEmployeeWage {
+    private List<EmployeeWageBuilder> employeeWageBuilderList = new ArrayList<>();
+
+    public List<EmployeeWageBuilder> getEmployeeWageBuilderList() {
+        return employeeWageBuilderList;
+    }
+}
+
